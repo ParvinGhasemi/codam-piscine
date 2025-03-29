deb82b7bc42133788d29f3a0d637ca056fae1b36
parvinghasemi <mghasemi.parvin@gmail.com>
2025-03-29 01:56:23 +0100

    edited main for function declaration

--- C00/ex06/main.c
+++ C00/ex06/main.c
@@ -6,11 +6,14 @@
 /*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/29 01:22:56 by parvinm.gha   #+#    #+#                 */
-/*   Updated: 2025/03/29 01:23:02 by parvinm.gha   ########   odam.nl         */
+/*   Updated: 2025/03/29 01:55:18 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+void	ft_print_comb2(void);
 
 int	main(void)
 {
 	ft_print_comb2();
+	return (0);
 }
