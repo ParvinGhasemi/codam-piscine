8b41745857796154e9acdcc43588550fc8973a9f
parvinghasemi <mghasemi.parvin@gmail.com>
2025-03-30 22:27:19 +0200

    added ft.h & main.c

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft.h                                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/03/29 16:27:56 by parvinm.gha   #+#    #+#                 */
+/*   Updated: 2025/03/30 22:26:56 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
