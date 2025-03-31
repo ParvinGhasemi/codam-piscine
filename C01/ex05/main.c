0cbbd263139f875fb3a051b27fb4411b40f2ab28
parvinghasemi <mghasemi.parvin@gmail.com>
2025-03-31 20:51:18 +0200

    add C01/ex05 ft_putstr.c & main.c

---
+++ C01/ex05/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/03/31 20:47:46 by parvinm.gha   #+#    #+#                 */
+/*   Updated: 2025/03/31 20:50:41 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putstr(char *str);
+
+int	main(void)
+{
+	char	*my_str;
+
+	my_str = "Hello World!";
+	ft_putstr(my_str);
+	return (0);
+}
