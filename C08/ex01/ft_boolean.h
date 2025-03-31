4f59d288ea2ada0e66b4f411005a1644924ae60d
parvinghasemi <mghasemi.parvin@gmail.com>
2025-03-31 02:43:52 +0200

    modified ft_boolean.h: using enum now

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/29 16:30:29 by parvinm.gha   #+#    #+#                 */
-/*   Updated: 2025/03/29 23:01:54 by parvinm.gha   ########   odam.nl         */
+/*   Updated: 2025/03/31 02:40:37 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 
 # include <unistd.h>
 
-typedef int	t_bool;
+typedef enum e_bool
+{
+	FALSE = 0,
+	TRUE = 1
+}	t_bool;
 
 # define EVEN(number) (number % 2 == 0)
 
-# define FALSE 0
-# define TRUE 1
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
