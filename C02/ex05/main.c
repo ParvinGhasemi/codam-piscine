52d384a4874a0e1f35ad399ffa780ef9d0a3f9d4
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-03 16:29:30 +0200

    C02/ex05 norminette check on main.c

--- C02/ex05/main.c
+++ C02/ex05/main.c
@@ -6,7 +6,7 @@
 /*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/01 01:04:12 by parvinm.gha   #+#    #+#                 */
-/*   Updated: 2025/04/03 16:14:59 by parvinm.gha   ########   odam.nl         */
+/*   Updated: 2025/04/03 16:28:50 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int	ft_str_is_uppercase(char *str);
 
 int	main(void)
 {
-	char string[] = "AAAA";
+	char	*string;
+
+	string = "AAAA";
 	printf("Is %s all uppercase?	0: No & 1: Yesss!\
 	>>	%d\n", string, ft_str_is_uppercase(string));
 }
