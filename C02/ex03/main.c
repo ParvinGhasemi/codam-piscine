05f12fd7643279245e4fc6376eecd51e0f395fd2
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-03 16:27:40 +0200

    C02/ex03 norminette check on main.c

--- C02/ex03/main.c
+++ C02/ex03/main.c
@@ -6,7 +6,7 @@
 /*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/01 00:55:22 by parvinm.gha   #+#    #+#                 */
-/*   Updated: 2025/04/03 04:05:58 by parvinm.gha   ########   odam.nl         */
+/*   Updated: 2025/04/03 16:26:26 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int	ft_str_is_numeric(char *str);
 
 int	main(void)
 {
-	char	mystr[] = "12345872643";
+	char	*mystr;
+
+	mystr = "12345872643";
 	printf("Is %s all digits??	[0: No & 1: Yesss!]\
 	>>	%d\n", mystr, ft_str_is_numeric(mystr));
 }
