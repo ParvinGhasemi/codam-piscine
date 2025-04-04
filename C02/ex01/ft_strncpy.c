61ffaa75ba9b30cafd918c44a4bb0405c9e49515
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-04 23:35:52 +0200

    add C02/ex01/ ft_strncpy.c

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strncpy.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmohamma <pmohamma@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/02/26 14:58:23 by pmohamma      #+#    #+#                 */
+/*   Updated: 2025/04/03 16:47:54 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
---
+++ C02/ex01/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/03 16:36:11 by parvinm.gha   #+#    #+#                 */
+/*   Updated: 2025/04/03 16:42:07 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+
+int	main(void)
+{
+	// char	text[] = "1$234567  Hello$ World!";
+	// char	copied[100] = "aaaa bbbb cccc dddd eeee ffff ";
+	char text[] = "1234567890";
+	char copied[100] = "abcdefghijklmnop";
+
+	printf("Before	>> text : %s	||	copied : %s\n", text, copied);
+	ft_strncpy(copied, text, 17);
+	printf("After	>> text : %s	||	copied : %s\n", text, copied);
+
+	return (0);
+}
