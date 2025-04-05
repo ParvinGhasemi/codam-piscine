b4d0f8079fda2b31ae919bd6a9aaf285b2326a37
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-05 05:39:21 +0200

    C02/ex09 add helper functions to ft_strcapitalize

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,34 +6,40 @@
 /*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/05 00:23:37 by parvinm.gha   #+#    #+#                 */
-/*   Updated: 2025/04/05 05:26:47 by parvinm.gha   ########   odam.nl         */
+/*   Updated: 2025/04/05 05:38:16 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+static int	ft_is_alnum(char character)
+{
+	return ((character >= '0' && character <= '9')
+			|| (character >= 'a' && character <= 'z')
+			|| (character >= 'A' && character <= 'Z'));
+}
+
+static int	ft_is_new_word(char *str, int i)
+{
+	if (i == 0)
+		return (1);
+	return (!ft_is_alnum(str[i - 1]));
+}
 
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (str[i] && str[i] >= 'a' && str[i] <= 'z')
-	{
+	if (str[i] && (str[i] >= 'a' && str[i] <= 'z'))
 		str[i] -= 32;
-		i++;
-	}
+	i++;
 	while (str[i])
 	{
 		if ((str[i] >= 'a' && str[i] <= 'z')
 			|| (str[i] >= 'A' && str[i] <= 'Z'))
 		{
-			if ((!((str[i - 1] >= '0' && str[i - 1] <= '9')
-						|| (str[i - 1] >= 'a' && str[i - 1] <= 'z')
-						|| (str[i - 1] >= 'A' && str[i - 1] <= 'Z')))
-				&& (str[i] >= 'a' && str[i] <= 'z'))
+			if (ft_is_new_word(str, i) && (str[i] >= 'a' && str[i] <= 'z'))
 				str[i] -= 32;
-			else if (((str[i - 1] >= '0' && str[i - 1] <= '9')
-					|| (str[i - 1] >= 'a' && str[i - 1] <= 'z')
-					|| (str[i - 1] >= 'A' && str[i - 1] <= 'Z'))
-				&& (str[i] >= 'A' && str[i] <= 'Z'))
+			else if (!ft_is_new_word(str, i) && (str[i] >= 'A' && str[i] <= 'Z'))
 				str[i] += 32;
 		}
 		i++;
