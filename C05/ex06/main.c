a7deb13229bb7d0a4fbd0720ad302c92edf483de
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-13 01:06:20 +0200

    adding main.c norminette checked to C05/ex06

--- C05/ex06/main.c
+++ C05/ex06/main.c
@@ -6,7 +6,7 @@
 /*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/13 01:01:12 by parvinm.gha   #+#    #+#                 */
-/*   Updated: 2025/04/13 01:02:32 by parvinm.gha   ########   odam.nl         */
+/*   Updated: 2025/04/13 01:05:56 by parvinm.gha   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 int	ft_is_prime(int num);
 
-int main() {
-	int	num = 30;
+int	main(void)
+{
+	int	num;
+
+	num = 30;
 	printf("Is %d prime? %d\n", num, ft_is_prime(num));
 	num = 2;
 	printf("Is %d prime? %d\n", num, ft_is_prime_recursive(num));
@@ -23,5 +26,5 @@ int main() {
 	printf("Is %d prime? %d\n", num, ft_is_prime(num));
 	num = 19;
 	printf("Is %d prime? %d\n", num, ft_is_prime(num));
-	return 0;
+	return (0);
 }
