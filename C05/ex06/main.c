66e6a25e0c97b633c7d2c40cc6d9941eadd4f777
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-13 01:04:44 +0200

    adding main.c to C05/ex06

---
+++ C05/ex06/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: parvinm.ghasemi <parvinm.ghasemi@studen      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/13 01:01:12 by parvinm.gha   #+#    #+#                 */
+/*   Updated: 2025/04/13 01:02:32 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_is_prime(int num);
+
+int main() {
+	int	num = 30;
+	printf("Is %d prime? %d\n", num, ft_is_prime(num));
+	num = 2;
+	printf("Is %d prime? %d\n", num, ft_is_prime_recursive(num));
+	num = 7;
+	printf("Is %d prime? %d\n", num, ft_is_prime(num));
+	num = 19;
+	printf("Is %d prime? %d\n", num, ft_is_prime(num));
+	return 0;
+}
