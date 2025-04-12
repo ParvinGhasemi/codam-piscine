fd3e08ebf2c0937c0c63aad0246deea3723739de
parvinghasemi <mghasemi.parvin@gmail.com>
2025-04-13 01:39:36 +0200

    adding C06/ex06  ft_sort_params.c

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_sort_params.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmohamma <pmohamma@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/03/10 11:53:29 by pmohamma      #+#    #+#                 */
+/*   Updated: 2025/04/13 01:36:49 by parvinm.gha   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	write(1, str, len);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	sort_params(int argc, char *argv[])
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+
+	sort_params(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
